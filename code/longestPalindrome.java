95b3299ec19e599067aa45d51842ec85e57e9828
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-06-17 09:13:16 -0400

    Create longestPalindrome.java

---
+++ code/longestPalindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length() < 2)
+            return s;
+        String longest = s.substring(0,1);
+        String tmp = s.substring(0,1);
+        for(int i = 0 ; i < s.length() ; i++){
+            tmp = helper(s,i,i);
+            if(tmp.length() > longest.length())
+                longest = tmp;
+            tmp = helper(s,i,i+1);
+            if(tmp.length() > longest.length())
+                longest = tmp;  
+        }
+        return longest;
+    }
+    
+    public String helper(String s,int begin,int end){
+        while(begin >=0 && end<= s.length()-1 && s.charAt(begin) == s.charAt(end)){
+            begin --;
+            end ++;
+        }
+        return s.substring(begin+1,end);
+    }
+        
+}
