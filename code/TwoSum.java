455c238aba391457830700144b272c5c29fcaddc
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-06-16 22:22:46 -0400

    Update and rename empty to TwoSum.java

---
+++ code/TwoSum.java
@@ -0,0 +1,36 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        int l = nums.length-1;
+        int s = 0;
+        //find the expected values
+        int[] nums2 = Arrays.copyOf(nums,nums.length);
+        Arrays.sort(nums2);
+        while(1==1){
+            if(nums2[s] + nums2[l] == target)
+                break;
+            else if(nums2[s] + nums2[l] < target)
+                s++;
+            else
+                l--;
+        }
+        //find the index
+        int flag = 0;
+        for(int i=0;i< nums.length;i++){
+            if(nums[i] == nums2[s]){
+                ans[0] = i;
+                flag += 1;
+            }
+            if(nums[nums.length -1 - i] == nums2[l] ){
+                ans[1] = nums.length -1 - i;
+                flag += 1;
+            }
+            
+            if(flag == 2)
+                break;
+
+        }
+        
+        return ans;
+    }
+}
