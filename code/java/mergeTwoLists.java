f0b5f6b3985dc2987e6ce5b95e72a8eb77d36167
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-06-21 17:09:13 -0400

    Create mergeTwoLists.java

---
+++ code/java/mergeTwoLists.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode l = new ListNode(0);
+        ListNode dummy = new ListNode(0);
+        dummy = l; 
+        if(l1 == null && l2 == null)
+            return null;
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        if (l1.val < l2.val){
+            l.next = l1;
+            l1 = l1.next;
+        }
+        else{
+            l.next = l2;
+            l2 = l2.next;
+        }
+        l = l.next;
+        
+        while (l1 != null || l2 != null){
+            
+            if(l1 != null && l2 != null){
+
+                if(l1.val > l2.val){
+                    l.next = l2;
+                    l2 = l2.next;
+                }
+                else{
+                    l.next = l1;
+                    l1 = l1.next;  
+                }
+            }
+            else if(l1 == null && l2 != null){
+                l.next = l2;
+                l2 = l2.next;
+            }
+                
+            else if(l1 != null && l2 == null){
+                l.next = l1;
+                l1=l1.next;
+            }
+            l = l.next;
+        }
+        return dummy.next;
+    }
+}
