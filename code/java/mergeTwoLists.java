5ad35504e053136a9d48d19e98e08a7d98408d0a
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-06-21 17:27:49 -0400

    Update mergeTwoLists.java

--- code/java/mergeTwoLists.java
+++ code/java/mergeTwoLists.java
@@ -17,15 +17,7 @@ class Solution {
             return l2;
         if(l2 == null)
             return l1;
-        if (l1.val < l2.val){
-            l.next = l1;
-            l1 = l1.next;
-        }
-        else{
-            l.next = l2;
-            l2 = l2.next;
-        }
-        l = l.next;
+
         
         while (l1 != null || l2 != null){
             
