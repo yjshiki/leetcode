4469d17d12cb64aa17436dc1c2818a2b1bc9e963
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-06-28 14:24:50 -0400

    Create threeSum.py

---
+++ code/java/threeSum.py
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        int j,r,s;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            if(i!=0 && nums[i] == nums[i-1])
+                continue;
+            j = i +1;
+            r = nums.length - 1;
+            while (j < r){
+                s = nums[i]+nums[j]+nums[r];
+                if(s>0)
+                    r--;
+                else if(s<0)
+                    j++;
+                else{
+                    res.add(Arrays.asList(nums[i],nums[j],nums[r]));
+                    while(j<r && nums[j]==nums[j+1])
+                        j++;
+                    while(j<r && nums[r]==nums[r-1])
+                        r--;
+                    j++;
+                    r--;
+                }
+            }
+        }
+        
+        
+        return res;
+    }
+}
