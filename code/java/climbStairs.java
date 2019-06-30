1af85473ce36c059308074d947ffdbb9698b4974
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-06-30 19:30:49 -0400

    Update climbStairs.java

--- code/java/climbStairs.java
+++ code/java/climbStairs.java
@@ -10,12 +10,12 @@ class Solution {
         if(n<=3)
             return n;
         
-        int[] nums = new int[n+1];
+        int[] nums = new int[n];
         nums[0] = 1;
-        nums[1] = 1;
-        for(int i=2;i<n+1;i++)
+        nums[1] = 2;
+        for(int i=2;i<n;i++)
             nums[i] = nums[i-1] + nums[i-2];
         
-        return nums[n];
+        return nums[n-1];
     }
 }
