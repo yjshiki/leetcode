59066e260c33a062bafbd04e225df0df66641df0
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-07-01 15:25:35 -0400

    Create uniquePathsWithObstacles.java

---
+++ code/java/uniquePathsWithObstacles.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                if(obstacleGrid[i][j]!= 1)
+                    obstacleGrid[i][j] = 1;
+                else
+                    obstacleGrid[i][j] = 0;
+        
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                if(obstacleGrid[i][j]!=0)
+                    if(i==0 & j != 0)
+                        obstacleGrid[i][j] = obstacleGrid[i][j-1];                    
+                    else if(i !=0 & j == 0)
+                        obstacleGrid[i][j] = obstacleGrid[i-1][j];
+                    else if(i !=0 & j!=0)
+                        obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+            
+            
+        return obstacleGrid[m-1][n-1];
+    }
+}
