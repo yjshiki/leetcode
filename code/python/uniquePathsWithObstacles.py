c375b0baef7e2511c42f5d6d54448b2253d85dc8
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-07-01 16:15:55 -0400

    Create uniquePathsWithObstacles.py

---
+++ code/python/uniquePathsWithObstacles.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[0])):
+                if obstacleGrid[i][j] == 1:
+                    obstacleGrid[i][j] = 0
+                else:
+                    obstacleGrid[i][j] = 1
+            
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[0])):
+                if(obstacleGrid[i][j] != 0):
+                    if(i == 0 and j !=0):
+                        obstacleGrid[i][j] = obstacleGrid[i][j-1]
+                    elif(i!=0 and j == 0):
+                        obstacleGrid[i][j] = obstacleGrid[i-1][j]
+                    elif(i!=0 and j!=0):
+                        obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+        
+        return obstacleGrid[-1][-1]
