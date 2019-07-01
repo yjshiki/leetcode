be2500f926dd6e3de74c63194cae03182a06cacc
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-07-01 13:55:53 -0400

    Update uniquePaths.java

--- code/java/uniquePaths.java
+++ code/java/uniquePaths.java
@@ -1,3 +1,4 @@
+//solution1
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] nums = new int[m][n];
@@ -9,5 +10,20 @@ class Solution {
                     nums[i][j] = nums[i-1][j] + nums[i][j-1];
         return nums[m-1][n-1];
                     
+    }
+}
+//solution2
+class Solution {
+    public int uniquePaths(int m, int n) {
+        long res = 1;
+        m = m-1;
+        n = n-1;
+        int total = m+n;
+        int small = Math.min(m,n);
+        for(int i=1;i<=small;i++){
+            res *= (m+n+1-i);
+            res /= i;
+        }
+        return (int)res;
     }
 }
