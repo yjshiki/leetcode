fc90b3fe85658c9a6ada0c67d9c3ac943ae79d95
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-07-05 17:04:26 -0400

    Update twoSumII.py

--- code/python/twoSumII.py
+++ code/python/twoSumII.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSumII(self, numbers: List[int], target: int) -> List[int]:
         i = 0;
         j = len(numbers) - 1
         while i != j:
