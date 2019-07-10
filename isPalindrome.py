aad72c28c284f962e16c13fc043ab7f6709cfdbc
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-07-10 17:18:31 -0400

    Create isPalindrome.py

---
+++ isPalindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        s = s.lower()
+        s1 = ""
+        s2 = ""
+        for i in range(len(s)):
+            if (s[i] >= 'a' and s[i] <= 'z') or (s[i] >= '0' and s[i] <= '9'):
+                s1 = s[i] + s1
+                s2 = s2 + s[i]
+        return s1==s2
