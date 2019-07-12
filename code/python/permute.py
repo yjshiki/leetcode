4e511720102417f3383800ddd86c9c387c676b89
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-07-12 17:40:50 -0400

    Create permute.py

---
+++ code/python/permute.py
@@ -0,0 +1,31 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return []
+        if len(nums) == 1:
+            return [nums]
+        lst = [[nums[0]]]
+        newlst = []
+        for i in range(1,len(nums)):
+            for j in range(len(lst)):
+                temp = self.insert(lst[j],nums[i])
+                newlst.extend(temp)
+            lst = newlst
+            newlst = []
+        return lst
+    
+    def insert(self,lst:List[int],num:int) -> List[List[int]]:
+        A = []
+        B = [num]
+        C = []
+        for i in range(len(lst)):
+            C.extend(lst[0:i])
+            C.extend(B)
+            C.extend(lst[i:len(lst)])
+            A.append(C.copy())
+            C = []
+        lst.extend(B)
+        A.append(lst)
+        return A
+
+        
