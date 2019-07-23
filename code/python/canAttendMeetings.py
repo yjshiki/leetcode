dc9788c9898bbad98333b0a85fdd0adc12c6edfb
yjshiki <35686303+yjshiki@users.noreply.github.com>
2019-07-23 10:48:45 -0400

    Create canAttendMeetings.py

---
+++ code/python/canAttendMeetings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) <= 1:
+            return True
+        intervals.sort(key=lambda x: x[0])
+        for i in range(len(intervals)-1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+        
+        return True
