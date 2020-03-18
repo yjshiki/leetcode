2c4584759f874aa5498e0bf0edf12b1214220f3e
yjshiki <35686303+yjshiki@users.noreply.github.com>
2020-03-18 18:38:26 -0400

    Create bstToGst.java

---
+++ code/java/bstToGst.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        helper(root,0);
+        return root;
+    }
+    
+    
+    public int helper(TreeNode root, int total){
+        if(root == null)
+            return total;
+        root.val += helper(root.right,total);
+        return helper(root.left,root.val);
+    }
+}
+
+
