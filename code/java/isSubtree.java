cfad0c4c31080ef09ce25c00251b064a089369c3
yjshiki <35686303+yjshiki@users.noreply.github.com>
2020-03-29 18:46:43 -0400

    Create isSubtree.java

---
+++ code/java/isSubtree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(t == null)
+            return true;
+        if(s == null)
+            return false;
+        if(helper(s,t) == false)
+            return (isSubtree(s.left,t) || isSubtree(s.right,t));
+        return true;
+    }
+    
+    public boolean helper(TreeNode s, TreeNode t){
+        if((s==null) && (t==null))
+            return true;
+        if((s==null) || (t==null))
+            return false;
+        if(s.val != t.val)
+            return false;
+        return (helper(s.left,t.left) && helper(s.right,t.right));
+    }
+}
